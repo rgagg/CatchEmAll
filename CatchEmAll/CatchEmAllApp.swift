f8a6169835885c396c9f2e7eb86b9a209cd53b2a
Richard Gagg <r.gagg@outlook.com>
2025-03-07 14:18:38 +1100

    Initial Commit

---
+++ CatchEmAll/CatchEmAllApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchEmAllApp.swift
+//  CatchEmAll
+//
+//  Created by Richard Gagg on 7/3/2025.
+//
+
+import SwiftUI
+
+@main
+struct CatchEmAllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
